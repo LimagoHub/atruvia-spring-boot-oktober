89768f0400b84693e11d39cb0305d334345444d0
Jo Wagner <jowagner@limago.de>
2021-10-12 13:41:06 +0200

    Errorhandling

--- webapp/src/main/java/de/atruvia/webapp/controllers/PersonenController.java
+++ webapp/src/main/java/de/atruvia/webapp/controllers/PersonenController.java
@@ -1,20 +1,26 @@
 package de.atruvia.webapp.controllers;
 
+
 import java.util.List;
 import java.util.Optional;
+import java.util.UUID;
 
 import javax.validation.Valid;
 
+import org.aspectj.weaver.patterns.HasThisTypePatternTriedToSneakInSomeGenericOrParameterizedTypePatternMatchingStuffAnywhereVisitor;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import de.atruvia.webapp.controllers.dtos.PersonDTO;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
@@ -80,5 +86,40 @@ public class PersonenController {
 		System.out.println("Person wurde gespeichert oder eingefuegt: " + dto);
 		return ResponseEntity.ok().build();
 	}
+
+//	@ApiResponse(responseCode = "201", description = "Person neu angelegt")
+//	@ApiResponse(responseCode = "200", description = "Person wurde geaendert")
+//	@ApiResponse(responseCode = "400", description = "Falsches Format")
+//	@ApiResponse(responseCode = "500", description = "interner Serverfehler")
+//	@PostMapping(path="", consumes = MediaType.APPLICATION_JSON_VALUE)
+//	public ResponseEntity<Void> saveOrUpdateNichtIdempotent(@RequestBody PersonDTO dto, UriComponentsBuilder builder) {
+//		dto.setId(UUID.randomUUID().toString());
+//		
+//		var uri = builder.path("/v1/personen/{id}").buildAndExpand(dto.getId());
+//		
+//		System.out.println("Person wurde gespeichert oder eingefuegt: " + dto);
+//		
+//		//return ResponseEntity.created(uri.toUri()).build();
+//		return ResponseEntity.ok().header(HttpHeaders.LOCATION, uri.toString()).build();
+//	}
+	
+//	// Ersatzget wegen Parameter OBJECT
+//	@PostMapping(path="/toUpper", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+//	public ResponseEntity<PersonDTO> toUpper (@RequestBody PersonDTO dto) {
+//		dto.setVorname(dto.getVorname().toUpperCase());
+//		dto.setNachname(dto.getNachname().toUpperCase());
+//		return ResponseEntity.ok(dto);
+//	}
+	
+		@GetMapping(path="/{id}/toUpper", produces = MediaType.APPLICATION_JSON_VALUE)
+		public ResponseEntity<PersonDTO> toUpper (@PathVariable String id) {
+			var dto = PersonDTO.builder().id(id).vorname("JOHN").nachname("DOE").build();
+			return ResponseEntity.ok(dto);
+		}
+		
+		@GetMapping(path="/{id}/toLower", produces = MediaType.APPLICATION_JSON_VALUE)
+		public ResponseEntity<PersonDTO> toLower (@PathVariable String id) {
+			throw new ArrayIndexOutOfBoundsException(5);
+		}
 
 }
